f6c5a20fa55d23f4a4115b5c34f2f4662c716c11
Ilya Sysoi <14ilya12322@gmail.com>
2018-12-03 10:56:24 +0300

    add book item

--- LibraryApp/BookListViewController.swift
+++ LibraryApp/BookListViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
-import SnapKit
 
 class BookListViewController: UIViewController {
 
+    private var booksTableView: UITableView!
+    private var books: [BookItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
